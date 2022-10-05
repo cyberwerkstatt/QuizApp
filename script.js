246371d8f2ce5c44fd07e69f75cd60b270901c50
cyberwerkstatt <seslija.sasha@gmail.com>
2022-10-05 10:00:10 +0200

    creating more funkctions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let questions = [
+let question1 = [
     {
         "question": "Wer hat HTML erfunden?",
         "answer_1":"Robbie Williams",
@@ -9,14 +9,29 @@ let questions = [
     }
 ];
 
+let question2 = [
+    {
+        "question": "Wer xxxxx?",
+        "answer_1":"Antwort1",
+        "answer_2":"Antwort2",
+        "answer_3":"Antwort3",
+        "answer_4":"Antwort4",
+        "right_answer": 4   
+    }
+];
+
 function startQuiz(){
-    document.getElementById("question").innerHTML = `<h2>${questions[0]["question"]}<h2>`;
+    document.getElementById("question").innerHTML = `<h2>${question1[0]["question"]}<h2>`;
     for(let i = 1; i <= 4; i++){
-        document.getElementById("list-questions").innerHTML += `<a class="list-group-item href="#">${questions[0]["answer_"+i]}</a>` 
+        document.getElementById("list-questions").innerHTML += `<a class="list-group-item href="#">${question1[0]["answer_"+i]}</a>` 
     }
 }
 
 function checkAnswer(){
+
+}
+
+function nextQuestion() {
     
 }
 
