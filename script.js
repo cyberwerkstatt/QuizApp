c7f20707f2ae095f75c4db79789453abee1b485e
cyberwerkstatt <seslija.sasha@gmail.com>
2022-10-05 13:27:46 +0200

    creating more funkctions pat 2

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-let question1 = [
+let questions = [
     {
         "question": "Wer hat HTML erfunden?",
         "answer_1":"Robbie Williams",
         "answer_2":"Lady Gaga",
         "answer_3":"Tim Berners-Lee",
         "answer_4":"Justin Bieber",
-        "right_answer": 3   
-    }
-];
+        "right_answer": 3
+    },  
 
-let question2 = [
     {
         "question": "Wer xxxxx?",
+        "answer_1":"Antwort1",
+        "answer_2":"Antwort2",
+        "answer_3":"Antwort3",
+        "answer_4":"Antwort4",
+        "right_answer": 4   
+    },
+
+    {
+        "question": "Wer 3?",
+        "answer_1":"Antwort1",
+        "answer_2":"Antwort2",
+        "answer_3":"Antwort3",
+        "answer_4":"Antwort4",
+        "right_answer": 4   
+    },
+
+    {
+        "question": "Wer x4?",
         "answer_1":"Antwort1",
         "answer_2":"Antwort2",
         "answer_3":"Antwort3",
@@ -20,11 +36,21 @@ let question2 = [
     }
 ];
 
-function startQuiz(){
-    document.getElementById("question").innerHTML = `<h2>${question1[0]["question"]}<h2>`;
-    for(let i = 1; i <= 4; i++){
-        document.getElementById("list-questions").innerHTML += `<a class="list-group-item href="#">${question1[0]["answer_"+i]}</a>` 
-    }
+let counter = 0;
+
+
+function init(){
+    document.getElementById("all-questions").innerHTML = questions.length;
+    startQuiz(counter);
+}
+
+
+
+
+ 
+function startQuiz(counter){
+    document.getElementById("questions").innerHTML = `<h2>${questions[counter]["question"]}</h2>`;
+    
 }
 
 function checkAnswer(){
@@ -32,6 +58,9 @@ function checkAnswer(){
 }
 
 function nextQuestion() {
-    
+    counter += 1;
+    startQuiz(counter);
+    if (counter > 3){
+        counter = 0;
+    }
 }
-
